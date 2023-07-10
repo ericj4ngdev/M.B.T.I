7769464d1b4d0eec8a42af06423da2cd1eb446cc
ericj4ngdev <ericjangdev@gmail.com>
2023-07-10 22:33:42 +0900

    teleport to building

---
+++ M.B.T.I/Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+[System.Serializable]
+public class DefaultRoom
+{
+    public string Name;
+    public int sceneIndex;
+    public int maxPlayer;
+}
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public List<DefaultRoom> defaultRooms;
+    public GameObject roomUI;
+
+    public void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect To Server...");
+    }
+
+    // 방에 들어오면 콜백되는 함수
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Server!!");
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("WE JOINED THE LOBBY");
+        roomUI.SetActive(true);
+    }
+
+    public void InitializeRoom(int defaultRoomIndex)
+    {
+        DefaultRoom roomsettings = defaultRooms[defaultRoomIndex];
+        
+        // 씬 로드
+        PhotonNetwork.LoadLevel(roomsettings.sceneIndex);
+        
+        // 방 생성
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = roomsettings.maxPlayer;
+        roomOptions.IsVisible = true;       // 참가자들이 볼수 있다.
+        roomOptions.IsOpen = true;          // 방이 열려있다.
+        
+        // 방과 연결
+        PhotonNetwork.JoinOrCreateRoom(roomsettings.Name, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
---
+++ M.B.T.I/Assets/Scripts/Network/NetworkPlayer.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Photon.Pun;
+using Unity.XR.CoreUtils;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+    private PhotonView photonView;
+
+    public Animator leftHandAnimator;
+    public Animator rightHandAnimator;
+
+
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        // 로컬상에 있는 XROrigin과 연결시킨다. 
+        XROrigin origin = FindObjectOfType<XROrigin>();
+        headRig = origin.transform.Find("Camera Offset/Main Camera");
+        leftHandRig = origin.transform.Find("Camera Offset/LeftHand Controller");
+        rightHandRig = origin.transform.Find("Camera Offset/RightHand Controller");
+        
+        //  네트워크 자기꺼 비활성화(대신 로컬상 XROrigin)
+        if (photonView.IsMine)
+        {
+            foreach (var item in GetComponentsInChildren<Renderer>())
+            {
+                item.enabled = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 클라이언트가 조종 가능하면
+        if (photonView.IsMine)
+        {
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
+
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand), leftHandAnimator);
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnimator);
+        }
+    }
+
+    void UpdateHandAnimation(InputDevice targetDevice, Animator handAnimator)
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+
+    void MapPosition(Transform target, Transform rigTransform)
+    {
+        // target = NetworkPlayer(네트워크)
+        // rigTransform = XROrigin(로컬)
+        target.position = rigTransform.position;
+        target.rotation = rigTransform.rotation;
+    }
+}
