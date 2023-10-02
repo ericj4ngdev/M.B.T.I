305612aa5d2564052c62b4e22290f50848428c6c
SSongBobby <rhd12wn12@naver.com>
2023-10-02 19:19:26 +0900

    feat-challengeManager
    add challengeManager

---
+++ M.B.T.I/Assets/Scripts/ChallengeManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeManager : MonoBehaviour
+{
+    private static int challengeCount;
+
+    [SerializeField]
+    private AudioSource completeSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("Challenges"))
+        {
+            Debug.Log("PlayerPref 생성");
+            challengeCount = 0;
+            PlayerPrefs.SetInt("Challenges", challengeCount);
+        }
+
+        completeSound = GetComponent<AudioSource>();
+    }
+    
+    public void CompleteChallenge()
+    {
+        PlayerPrefs.SetInt("Challenges", ++challengeCount);
+
+        if (PlayerPrefs.GetInt("Challenges") == 6)
+        {
+            Debug.Log("불꽃놀이");
+        }
+    }
+
+}
