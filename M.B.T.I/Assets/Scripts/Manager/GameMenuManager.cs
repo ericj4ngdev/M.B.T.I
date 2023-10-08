db388d3593e099df6b61097837e1e3abacf07480
ericj4ngdev <ericjangdev@gmail.com>
2023-10-08 19:54:07 +0900

    Add PCU_Button
    - 게임 메뉴 배치
    경희대, 전주대, 배재대, 계원예대, 건대에 배치 완료
    아직 도전과제 연동은 배재대만 되어있음
    - 배재대 완료
    지금은 시작하자마자 랜턴 돌아감

--- M.B.T.I/Assets/Scripts/Manager/GameMenuManager.cs
+++ M.B.T.I/Assets/Scripts/Manager/GameMenuManager.cs
@@ -1,34 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class GameMenuManager : MonoBehaviour
 {
     // 플레이어 앞에 UI띄우기 위한 변수 생성
-    // public Transform head;
-    // public float spawnDistance = 0.5f;
-    private GameObject XRPlayer;
+    private Transform head;
+    public float spawnDistance = 0.5f;
     public GameObject menu;
     public GameObject initScreen;
     public GameObject checkBox;
+    public GameObject startBtn;
     public InputActionProperty showButton;
 
     HashSet<string> completeChallenges = new HashSet<string>();
 
+
+    // 플레이어 처음엔 못움직이게 하기
+    private XROrigin xrOrigin;
+    
+
     private void Start()
     {
-        XRPlayer = FindObjectOfType<XROrigin>().gameObject;
+        if (xrOrigin == null) xrOrigin = FindObjectOfType<XROrigin>();
+        head = xrOrigin.Camera.transform;
+        SetMovement(false);
         InitializeChallenge();
     }
 
+
     private void InitializeChallenge()
     {
-        completeChallenges = ChallengeManager.GetInstance().getCompleteChallenges();
-        string currentSceneName = SceneManager.GetActiveScene().name;
+        completeChallenges = ChallengeManager.GetInstance().getCompleteChallenges();    // 완료한 캠퍼스 이름 HashSet
+        string currentSceneName = SceneManager.GetActiveScene().name;   // 현재 컷씬 이름
         string[] sceneNameParts = currentSceneName.Split(new char[] { '_' }); // 여기서는 언더스코어를 기준으로 분할합니다
         
         foreach (var extractedName in sceneNameParts)
@@ -43,35 +53,64 @@ public class GameMenuManager : MonoBehaviour
         {
             if (challengeName == campusName)
             {
-                // challengeName과 씬 이름이 일치하면 활성화
+                // challengeName과 씬 이름이 일치하면 완료한 곳이므로 메뉴 안띄움
+                // 이동이 자유로움
+                menu.SetActive(false);
                 checkBox.SetActive(true);
+                startBtn.SetActive(false);
                 initScreen.SetActive(false);
+                SetMovement(true);
                 break; // 이미 일치하는 요소를 찾았으므로 나머지 요소들을 확인할 필요가 없습니다.
-            }
+            }      
         }
+        // 여기에 도달했다면 아직 클리어하지 않은 캠퍼스이므로 움직임 비활성화
+
     }
 
+    void SetComponentEnabled<T>(bool isEnabled) where T : MonoBehaviour
+    {
+        MonoBehaviour componentToDisable = xrOrigin.GetComponent<T>();
+        componentToDisable.enabled = isEnabled;
+    }
+
+    // 시작 버튼 이벤트
+    public void SetMovement(bool canMove)
+    {
+        //SetComponentEnabled<ActionBasedContinuousMoveProvider>(canMove);
+        // 컴포넌트 비활성화하면 Null오류가 떠서 속도를 0으로 만들어버리려 한다. 
+        var move = xrOrigin.GetComponent<ActionBasedContinuousMoveProvider>();
+        move.moveSpeed = canMove ? 2 : 0;
+        var turn = xrOrigin.GetComponent<ActionBasedContinuousTurnProvider>();
+        turn.turnSpeed = canMove ? 60 : 0;
+        SetComponentEnabled<TeleportationProvider>(canMove);
+        SetComponentEnabled<ActivateTeleportationRay>(canMove);
+    }
 
     void Update()
     {
+        // 키보드용
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Menu");
             menu.SetActive(!menu.activeSelf);
         }
+
         if (showButton.action.WasPressedThisFrame())
         {
+            // 메뉴 활성화
             Debug.Log("Menu");
             menu.SetActive(!menu.activeSelf);
+
+            // 플레이어 앞에 띄우기
+            menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
         }
-        // menu.transform.position = head.position + new Vector3(head.forward.x, 0, head.forward.z).normalized * spawnDistance;
-        // menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
-        // menu.transform.forward *= -1;
+        menu.transform.LookAt(new Vector3(head.position.x, menu.transform.position.y, head.position.z));
+        menu.transform.forward *= -1;
     }
 
     public void TeleportToBuildingForward(GameObject SpawnSpot)
     {
-        XRPlayer.transform.position = SpawnSpot.transform.position;
+        xrOrigin.transform.position = SpawnSpot.transform.position;
     }
 
 }
--- M.B.T.I/Assets/Scripts/Network/PCUController.cs
+++ M.B.T.I/Assets/Scripts/Network/PCUController.cs
@@ -10,12 +10,34 @@ namespace PCU
     {
         public PCUCharactor pcuCharactor;         // 캐릭터 가져오기
         public Animator animator;
-        
+        public Material[] materials;
         
         private void Awake()
         {
             // 포톤 네트워크에 타입을 등록
             PhotonPeer.RegisterType(typeof(ColorData), 0, ColorData.Serialize, ColorData.Deserialize);
+        }
+
+        // 버튼을 누를 때마다 랜덤 색이 정해진다. 
+        public void SetRandomColorData()
+        {
+            ColorData custom = new ColorData();
+            Material material;
+            int temp = Random.Range(0, materials.Length - 1);
+            material = materials[temp];
+
+            // 캐릭터 색상 변경
+            custom.r = (material.color.r * 255);        // material 값은 0~1 사이 값(float)이다. 
+            custom.g = (material.color.g * 255);
+            custom.b = (material.color.b * 255);
+            custom.a = (material.color.a * 255);
+
+            Debug.Log("material.color.r : " + material.color.r + "\n" + "custom.r: " + custom.r);
+            Debug.Log("material.color.g : " + material.color.g + "\n" + "custom.g: " + custom.g);
+            Debug.Log("material.color.b : " + material.color.b + "\n" + "custom.b: " + custom.b);
+            Debug.Log("material.color.a : " + material.color.a + "\n" + "custom.a: " + custom.a);
+
+            pcuCharactor.SetColor(custom);
         }
 
         // 각 버튼에서 이 함수를 이벤트로 호출한다. 
